fd95d4f45ed06f8b286ef5468595a1d01d154c74
bxb1pro <bxb1pro@gmail.com>
2024-04-18 12:45:55 +0100

    Extra changes before Roles work
    Small modifications to maps, added an address to developers, small UI changes.

---
+++ backend/Migrations/20240418111916_databasemod5.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitalGamesMarketplace2.Migrations
+{
+    /// <inheritdoc />
+    public partial class databasemod5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Developers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Developers");
+        }
+    }
+}
